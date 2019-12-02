1d8d71974733c84ba88df32e997424ab43fde036
DimuthuHemachandra <dhemacha@uwo.ca>
2019-12-02 17:48:45 -0500

    workflow edited for recon all

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -1,10 +1,12 @@
 #This is a Nipype generator. Warning, here be dragons.
 #!/usr/bin/env python
 
+import os
 import sys
 import nipype
 import nipype.pipeline as pe
 import argparse
+from os.path import join as opj
 
 import nipype.interfaces.io as io
 import nipype.interfaces.freesurfer as freesurfer
@@ -41,19 +43,28 @@ else:
     subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
     sub_ids = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
 
+fs_folder = opj(out_dir, 'freesurfer')  # location of freesurfer folder
+os.system('mkdir -p %s'%fs_folder)
+
 #Flexibly collect data from disk to feed into workflows.
-NodeHash_1fc3610 = pe.Node(io.SelectFiles(templates={'T1':'{sub_id}/anat/{sub_id}_T1w.nii.gz'}), name = 'NodeName_1fc3610')
+NodeHash_1fc3610 = pe.Node(io.SelectFiles(templates={'T1':'sub-{sub_id}/anat/sub-{sub_id}_T1w.nii.gz'}), name = 'NodeName_1fc3610')
 NodeHash_1fc3610.inputs.base_directory = bids_dir
 NodeHash_1fc3610.iterables = [('sub_id', sub_ids)]
+#NodeHash_1fc3610.iterables = [('sub_id', sub_ids)]
 
 #Wraps the executable command ``recon-all``.
 freesurfer_recon_all = pe.Node(interface = freesurfer.ReconAll(), name='freesurfer_recon_all')
 freesurfer_recon_all.inputs.directive = "all"
+#freesurfer_recon_all.inputs.subject_id = sub_ids
+freesurfer_recon_all.inputs.subjects_dir = fs_folder
+freesurfer_recon_all.base_dir = opj(out_dir, 'work')
+#b = pe.Node(interface=B(), name="b")
+freesurfer_recon_all.iterables = ("subject_id", ["sub-"+s for s in sub_ids])
 
 #Generic datasink module to store structured outputs
 io_data_sink = pe.Node(interface = io.DataSink(), name='io_data_sink')
 io_data_sink.inputs.base_directory = out_dir
-io_data_sink.inputs.parameterization = True
+io_data_sink.inputs.subjects_dir = True
 
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
@@ -63,6 +74,6 @@ analysisflow.connect(NodeHash_1fc3610, "T1", freesurfer_recon_all, "T1_files")
 
 #Run the workflow
 plugin = 'MultiProc' #adjust your desired plugin here
-plugin_args = {'n_procs': 1} #adjust to your number of cores
-analysisflow.write_graph(graph2use='flat', format='png', simple_form=False)
+plugin_args = {'n_procs': 8} #adjust to your number of cores
+#analysisflow.write_graph(graph2use='flat', format='png', simple_form=False)
 analysisflow.run(plugin=plugin, plugin_args=plugin_args)
